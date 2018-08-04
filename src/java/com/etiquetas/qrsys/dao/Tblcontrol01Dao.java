ee6b9e35560de9a0141de4978d0121774030e5b7
danteac <danteac@dante-pc>
2018-08-04 08:23:12 -0500

    UpdateTbl67Control

--- src/java/com/etiquetas/qrsys/dao/Tblcontrol01Dao.java
+++ src/java/com/etiquetas/qrsys/dao/Tblcontrol01Dao.java
@@ -5,5 +5,7 @@ public interface Tblcontrol01Dao {
     public void updateTblControl(int clave);//ID_TABLE=57
     
     public void updateTablaControl(int clave);//ID_TABLE=48
+    
+    public void updateTbl67Control(int clave);//ID_TABLE=67
 
 }

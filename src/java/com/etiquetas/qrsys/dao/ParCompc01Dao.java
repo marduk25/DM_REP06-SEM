664027194e41bcc3689184437a33ff9bd924dd6a
danteac <danteac@dante-pc>
2018-08-06 16:42:43 -0500

    public List<ParCompc01> obtenerMaximoValor(String factura);

--- src/java/com/etiquetas/qrsys/dao/ParCompc01Dao.java
+++ src/java/com/etiquetas/qrsys/dao/ParCompc01Dao.java
@@ -1,9 +1,12 @@
 package com.etiquetas.qrsys.dao;
 
 import com.etiquetas.qrsys.s.model.ParCompc01;
+import java.util.List;
 
 public interface ParCompc01Dao {
 
     public void saveParCompc01(ParCompc01 parcompc);
+    
+    public List<ParCompc01> obtenerMaximoValor(String factura);
 
 }

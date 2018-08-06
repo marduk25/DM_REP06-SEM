f542048557ef1b555db52f620cd150be4f69fdb2
danteac <danteac@dante-pc>
2018-08-06 11:44:14 -0500

    Hnumser01Dao

---
+++ src/java/com/etiquetas/qrsys/dao/Hnumser01Dao.java
@@ -0,0 +1,9 @@
+package com.etiquetas.qrsys.dao;
+
+import com.etiquetas.qrsys.s.model.Hnumser01;
+import java.util.List;
+
+public interface Hnumser01Dao {
+
+    public List<Hnumser01> obtenerMaximoValor();
+}

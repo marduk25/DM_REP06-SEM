c46cfb9e7464fe031058247520ee5a742c54d28d
danteac <danteac@dante-pc>
2018-08-06 14:18:28 -0500

    ParCompcClib01Dao

---
+++ src/java/com/etiquetas/qrsys/dao/ParCompcClib01Dao.java
@@ -0,0 +1,11 @@
+
+package com.etiquetas.qrsys.dao;
+
+import com.etiquetas.qrsys.s.model.ParCompcClib01;
+
+
+public interface ParCompcClib01Dao {
+    
+    public void saveParCompcClib(ParCompcClib01 clib01);
+    
+}
